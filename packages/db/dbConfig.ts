eb6c4e6bc9a4ad153697e60bd0dae07b74bc8230
Suryansh Anand <spirit@anandsur.com>
2024-04-07 10:58:42 +0530

    feat: add DB_URL in drizzle

--- packages/db/dbConfig.ts
+++ packages/db/dbConfig.ts
@@ -4,7 +4,8 @@ export const dbConfig: Config = {
   schema: "../../packages/db/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: "postgres://postgres:admin@0.0.0.0:5432/postgres",
+    connectionString:
+      process.env.DB_URL || "postgres://postgres:admin@0.0.0.0:5432/postgres",
   },
   out: "./drizzle",
   verbose: true,
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -3,7 +3,8 @@ import { Pool } from "pg";
 import * as schema from "./schema";
 
 const pool = new Pool({
-  connectionString: "postgres://postgres:admin@0.0.0.0:5432/postgres",
+  connectionString:
+    process.env.DB_URL || "postgres://postgres:admin@0.0.0.0:5432/postgres",
 });
 
 export const db = drizzle(pool, { schema });
