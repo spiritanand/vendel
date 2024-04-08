b70c30f4f7ab48f69d0bb73b39bc27956b8502cb
Suryansh Anand <spirit@anandsur.com>
2024-04-08 22:36:36 +0530

    feat: landing page

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -1,10 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@repo/ui/ui/button";
 import Header from "@/components/layout/Landing/header.tsx";
+import SignIn from "@/components/actions/SignIn";
+import { auth } from "@/libWeb/auth";
+import { ROUTES } from "@/libWeb/constants.ts";
 
-function Page() {
+async function Page() {
+  const session = await auth();
+
   return (
     <>
       <Header />
-      <div>DESC</div>
+
+      <main className="container flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-primary text-center text-5xl font-black uppercase sm:text-6xl md:text-9xl">
+          Integrate web3 checkout
+        </h1>
+        <h2 className="text-primary mt-1 text-center text-3xl font-bold uppercase">
+          in your apps
+        </h2>
+        <h3 className="text-primary mt-5 flex items-center justify-center gap-3 text-center text-xl">
+          Powered by{" "}
+          <Image alt="solana" height={30} src="/sol-logo.svg" width={30} />
+        </h3>
+
+        {session ? (
+          <Link href={ROUTES.PRODUCTS}>
+            <Button className="mt-16 p-8 text-4xl font-black">
+              Go To Dashboard
+            </Button>
+          </Link>
+        ) : (
+          <SignIn className="mt-16 p-10 text-5xl font-black" />
+        )}
+      </main>
     </>
   );
 }
--- apps/web/src/components/layout/Landing/header.tsx
+++ apps/web/src/components/layout/Landing/header.tsx
@@ -1,9 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
 import { auth } from "@/libWeb/auth.ts";
 import { ROUTES } from "@/libWeb/constants.ts";
+import AvatarMenu from "@/components/actions/AvatarMenu.tsx";
 import SignIn from "../../actions/SignIn.tsx";
 
 export default async function Header() {
   const session = await auth();
 
-  return <>{!session && <SignIn callbackUrl={ROUTES.PRODUCTS} />}</>;
+  return (
+    <header className="absolute left-0 right-0 top-0 p-4">
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li>
+            <Link href="/">
+              <Image alt="Vendel" height={40} src="/logo.png" width={40} />
+            </Link>
+          </li>
+          {!session ? (
+            <li>
+              <SignIn callbackUrl={ROUTES.PRODUCTS} />
+            </li>
+          ) : null}
+
+          {session ? (
+            <>
+              <li className="text-2xl">
+                <Link href={ROUTES.PRODUCTS}>Dashboard</Link>
+              </li>
+              <li className="">
+                <AvatarMenu />
+              </li>
+            </>
+          ) : (
+            ""
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 }
